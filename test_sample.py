c4583ec7540405162d4f568b23b3c09494867272
Enrique <73123851+F4l4X@users.noreply.github.com>
2023-11-08 09:59:41 +0100

    test

--- test_sample.py
+++ test_sample.py
@@ -5,3 +5,7 @@ class TestUnit:
     def test_sum(self):
         res = rpn_reader("20 3 +")
         assert res == 23
+
+    def test_substract(self):
+        res = rpn_reader("4 2 -")
+        assert res == 2
