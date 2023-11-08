c1005460bfbb799c4758177e6bbefbc56f5fa03c
victorbillaud <victorbillaud@gmail.com>
2023-11-08 09:36:41 +0100

    resolve function

---
+++ app.py
@@ -0,0 +1,21 @@
+str = "20 2 +"
+
+
+def rpn_reader(expression: str) -> int:
+    items = str.split(" ")
+
+    stack = []
+
+    for item in items:
+        if item == "+":
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(b + a)
+        elif item == "-":
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(b - a)
+        else:
+            stack.append(int(item))
+
+    return stack.pop()
